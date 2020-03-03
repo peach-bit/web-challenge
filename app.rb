b7881dab1853ef0ca2e7d68fd4b7030d6082dd7f
Hodan <hodanahmed0000@gmail.com>
2020-03-03 15:52:32 +0000

    Added form element

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+configure do
+  set:session_secret, "cookie's website"
+
 get '/' do
   "Hello World"
 end
@@ -6,7 +9,16 @@ end
    "hehe"
  end
 
- get '/cat' do
-   @name = ["Amigo", "Oscar", "Viking"].sample
-   erb(:index)
- end
+get '/random-cat' do
+  @name = ["Cookie", "Fred", "Google"].sample
+  @color = ["Red","Blue","Green"].sample
+  erb(:index)
+end
+
+get '/namedcat' do
+  p params
+  @name = params[:name]
+  @color = params[:color]
+  erb(:index)
+end
+end
